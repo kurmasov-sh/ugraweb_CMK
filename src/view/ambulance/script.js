92584ff9502f9be6600ca44846b7a638a58ffeae
kurmasov <kurmasov-sh@ugraweb.ru>
2023-10-24 19:40:56 +0300

    исправления классов

--- src/view/ambulance/script.js
+++ src/view/ambulance/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+    let leftBtn = '<svg class="slider__arrow-l" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+        '<path d="M3.75 15L26.25 15L3.75 15ZM26.25 15L15.625 25.625L26.25 15ZM26.25 15L15.625 4.375L26.25 15Z" fill="#597D69"/>\n' +
+        '<path d="M3.75 15L26.25 15M26.25 15L15.625 25.625M26.25 15L15.625 4.375" stroke="#597D69" stroke-width="1.875" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+        '</svg>\n'
+    let rightBtn = '<svg class="slider__arrow-r" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+        '<path d="M3.75 15L26.25 15L3.75 15ZM26.25 15L15.625 25.625L26.25 15ZM26.25 15L15.625 4.375L26.25 15Z" fill="#597D69"/>\n' +
+        '<path d="M3.75 15L26.25 15M26.25 15L15.625 25.625M26.25 15L15.625 4.375" stroke="#597D69" stroke-width="1.875" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+        '</svg>\n'
     $(".owl-carousel").owlCarousel({
         items: 3,
         responsive: {
@@ -14,9 +22,9 @@ $(document).ready(function(){
         },
         margin: 40,
         nav: true,
-        navText: ['<span class="gallery__arrow-l"><img src="/src/img/f-arrow-r.svg" alt=""></span>', '<span class="gallery__arrow-r"><img src="/src/img/f-arrow-r.svg" alt=""></span>']
+        navText: [leftBtn, rightBtn]
     });
     let a = $('.owl-nav');
-    let top = $('.gallery__top');
+    let top = $('.slider__top');
     top.append(a);
 });
