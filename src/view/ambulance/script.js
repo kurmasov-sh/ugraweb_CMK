f72557af6a246f9c5649f3ccfd1099a89a685dda
kurmasov <kurmasov-sh@ugraweb.ru>
2023-10-24 17:06:19 +0300

    закончил адаптив

--- src/view/ambulance/script.js
+++ src/view/ambulance/script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
+        items: 3,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            767: {
+                items: 2,
+            },
+            1364: {
+                items: 3,
+            }
+        },
         margin: 40,
         nav: true,
         navText: ['<span class="gallery__arrow-l"><img src="/src/img/f-arrow-r.svg" alt=""></span>', '<span class="gallery__arrow-r"><img src="/src/img/f-arrow-r.svg" alt=""></span>']
