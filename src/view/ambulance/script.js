f0e2508eaa099036b274f90b67732f62bd0a7ee1
kurmasov <kurmasov-sh@ugraweb.ru>
2023-10-27 11:32:45 +0300

    исправил hover стрелок

--- src/view/ambulance/script.js
+++ src/view/ambulance/script.js
@@ -1,12 +1,6 @@
 $(document).ready(function(){
-    let leftBtn = '<svg class="slider__arrow-l" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
-        '<path d="M3.75 15L26.25 15L3.75 15ZM26.25 15L15.625 25.625L26.25 15ZM26.25 15L15.625 4.375L26.25 15Z" fill="#597D69"/>\n' +
-        '<path d="M3.75 15L26.25 15M26.25 15L15.625 25.625M26.25 15L15.625 4.375" stroke="#597D69" stroke-width="1.875" stroke-linecap="round" stroke-linejoin="round"/>\n' +
-        '</svg>\n'
-    let rightBtn = '<svg class="slider__arrow-r" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
-        '<path d="M3.75 15L26.25 15L3.75 15ZM26.25 15L15.625 25.625L26.25 15ZM26.25 15L15.625 4.375L26.25 15Z" fill="#597D69"/>\n' +
-        '<path d="M3.75 15L26.25 15M26.25 15L15.625 25.625M26.25 15L15.625 4.375" stroke="#597D69" stroke-width="1.875" stroke-linecap="round" stroke-linejoin="round"/>\n' +
-        '</svg>\n'
+    let leftBtn = '<div class="btn-arrow arrow-l"><span></span></div>'
+    let rightBtn = '<div class="btn-arrow arrow-r"><span></span></div>'
     $(".owl-carousel").owlCarousel({
         items: 3,
         responsive: {
